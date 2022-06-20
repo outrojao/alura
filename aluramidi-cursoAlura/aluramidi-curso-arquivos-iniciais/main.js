2654e9c6d3c1204a8fb24397f09b8ce37c6897f1
Sants16 <jvsantosrocha980@gmail.com>
2022-06-20 18:17:41 -0300

    20/06

--- aluramidi-cursoAlura/aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-cursoAlura/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,13 @@
-document.querySelector('#som_tecla_pom').play();
+function tocarPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+let tecla_pom = document.querySelector('.tecla_pom');
+tecla_pom.addEventListener('click', tocarPom)
+
+function tocarClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+let tecla_clap = document.querySelector('.tecla_clap');
+tecla_clap.addEventListener('click', tocarClap)
