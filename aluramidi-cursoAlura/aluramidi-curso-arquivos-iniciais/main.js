359763611095f5b09ba4aea45e19e8d26ac665b3
Sants16 <jvsantosrocha980@gmail.com>
2022-06-22 18:52:21 -0300

    22/06

--- aluramidi-cursoAlura/aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-cursoAlura/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,8 +1,10 @@
-function tocaPom(){
-    let somPom = document.querySelector('#som_tecla_pom');
-    somPom.play();
+function tocaSom (idTagAudio){ //cria uma função que irá tocar todos os audios afim de evitar a repetição do codigo, a escolha do som q deve ser tocado sera feito por parametros
+
+    document.querySelector(idTagAudio).play();
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla'); /* ao invés de capturarmos um elemento por vez, conseguimos pegar todos de uma só vez. Portanto isto facilitará a manipulação, reutilização e manutenção do nosso código para todos os elementos que recebem o mesmo tipo de função. */
 
-ListaDeTeclas[0].onclick = tocaPom;
+for (c = 0; c < ListaDeTeclas.length; c++){
+    ListaDeTeclas[c].onclick = tocaSom();
+}
